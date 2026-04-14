95c9fdc47189888cef694294bc9caaa560126487
Oleksandr <pyvko.a.g@gmail.com>
2026-04-15 02:49:22 +0300

    updated links after deployment

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ const app = express();
 // const PORT = 3000;
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://job-hunt-fgxu.vercel.app",
+  }),
+);
 app.use(express.json());
 app.use(applications);
 app.use("/auth", authRoutes);
