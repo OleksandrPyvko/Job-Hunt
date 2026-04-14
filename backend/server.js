97536987457bd2d68b3642b98a633e1a6fe488a6
Oleksandr <pyvko.a.g@gmail.com>
2026-04-15 01:59:23 +0300

    changed port for deploying

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ const applications = require("./applicationsRoutes");
 const authRoutes = require("./authRoutes");
 
 const app = express();
-const PORT = 3000;
+// const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -15,8 +16,8 @@ app.use("/auth", authRoutes);
 connect
   .connectToServer()
   .then(() => {
-    app.listen(PORT, () => {
-      console.log("Server is running on port 3000");
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server is running on ${PORT} `);
     });
   })
   .catch((error) => {
